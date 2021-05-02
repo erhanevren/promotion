e65694e11ec8c3f66ac1e28c251a3b638568dac7
erhanevren <erhan.evren@gmail.com>
2021-05-03 00:54:25 +0300

    Unit tests added for promotion calculation samples.

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -13,7 +13,7 @@ namespace UnitTestProject1
         public void TestMethod1()
         {
             Form1 ff = new Form1();
-            Assert.ThrowsException<System.FormatException>(() => ff.Create_Promotion(this, new EventArgs()));
+            //Assert.ThrowsException<System.FormatException>(() => ff.Create_Promotion(this, new EventArgs()));
         }
 
         [TestMethod]
@@ -37,6 +37,222 @@ namespace UnitTestProject1
             //Assert.ThrowsException<Exception>(() => ff.Select_SKU_For_Promotion(this, new EventArgs()));
         }
 
-        
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Basket basket = new Basket();
+            BasketItem basketItem1 = new BasketItem();
+            basketItem1.SKU = "A";
+            basketItem1.price = 50;
+            basket.BasketItemList.Add(basketItem1);
+
+            BasketItem basketItem2 = new BasketItem();
+            basketItem2.SKU = "A";
+            basketItem2.price = 50;
+            basket.BasketItemList.Add(basketItem2);
+
+            BasketItem basketItem3 = new BasketItem();
+            basketItem3.SKU = "B";
+            basketItem3.price = 60;
+            basket.BasketItemList.Add(basketItem3);
+
+            PromotionObj promotion = new PromotionObj();
+            /*List<PromotionType> PromotionList = new List<PromotionType>();
+            PromotionType promotionType = new PromotionType();
+            promotionType.operand = 50;
+            //promotionType.operatorKey = new List<string>();
+            //promotionType.operatorKey.Add("A");
+            promotionType.operatorType = "static";
+            promotionType.SKU_percentage = "";
+            promotion.PromotionList.Add(promotionType);*/
+
+            List<SKU_Item> SKU_PriceList = new List<SKU_Item>();
+
+            SKU_Item SKU_Item1 = new SKU_Item();
+            SKU_Item1.SKU = "A";
+            SKU_Item1.price = 50;
+            SKU_PriceList.Add(SKU_Item1);
+
+            SKU_Item SKU_Item2 = new SKU_Item();
+            SKU_Item2.SKU = "B";
+            SKU_Item2.price = 60;
+            SKU_PriceList.Add(SKU_Item2);
+
+            SKU_Item SKU_Item3 = new SKU_Item();
+            SKU_Item3.SKU = "C";
+            SKU_Item3.price = 40;
+            SKU_PriceList.Add(SKU_Item3);
+
+            List<string> AppliedPromotions;
+            int total = promotion.ApplyPromotion(basket, promotion.PromotionList, SKU_PriceList, out AppliedPromotions);
+
+            Assert.AreEqual(160, total);
+            //Assert.ThrowsException<NullReferenceException>(() => promotion.ApplyPromotion(basket, promotion.PromotionList, SKU_PriceList, out AppliedPromotions));
+
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Basket basket = new Basket();
+            BasketItem basketItem1 = new BasketItem();
+            basketItem1.SKU = "A";
+            basketItem1.price = 50;
+            basket.BasketItemList.Add(basketItem1);
+
+            BasketItem basketItem2 = new BasketItem();
+            basketItem2.SKU = "A";
+            basketItem2.price = 50;
+            basket.BasketItemList.Add(basketItem2);
+
+            BasketItem basketItem3 = new BasketItem();
+            basketItem3.SKU = "B";
+            basketItem3.price = 60;
+            basket.BasketItemList.Add(basketItem3);
+
+            PromotionObj promotion = new PromotionObj();
+            List<PromotionType> PromotionList = new List<PromotionType>();
+            PromotionType promotionType = new PromotionType();
+            promotionType.operand = 20;
+            promotionType.operatorKey = new List<string>();
+            promotionType.operatorKey.Add("A");
+            promotionType.operatorType = "static";
+            promotionType.SKU_percentage = "";
+            promotion.PromotionList.Add(promotionType);
+
+            List<SKU_Item> SKU_PriceList = new List<SKU_Item>();
+
+            SKU_Item SKU_Item1 = new SKU_Item();
+            SKU_Item1.SKU = "A";
+            SKU_Item1.price = 50;
+            SKU_PriceList.Add(SKU_Item1);
+
+            SKU_Item SKU_Item2 = new SKU_Item();
+            SKU_Item2.SKU = "B";
+            SKU_Item2.price = 60;
+            SKU_PriceList.Add(SKU_Item2);
+
+            SKU_Item SKU_Item3 = new SKU_Item();
+            SKU_Item3.SKU = "C";
+            SKU_Item3.price = 40;
+            SKU_PriceList.Add(SKU_Item3);
+
+            List<string> AppliedPromotions;
+            int total = promotion.ApplyPromotion(basket, promotion.PromotionList, SKU_PriceList, out AppliedPromotions);
+
+            Assert.AreEqual(130, total);
+            //Assert.ThrowsException<NullReferenceException>(() => promotion.ApplyPromotion(basket, promotion.PromotionList, SKU_PriceList, out AppliedPromotions));
+
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Basket basket = new Basket();
+            BasketItem basketItem1 = new BasketItem();
+            basketItem1.SKU = "A";
+            basketItem1.price = 50;
+            basket.BasketItemList.Add(basketItem1);
+
+            BasketItem basketItem2 = new BasketItem();
+            basketItem2.SKU = "A";
+            basketItem2.price = 50;
+            basket.BasketItemList.Add(basketItem2);
+
+            BasketItem basketItem3 = new BasketItem();
+            basketItem3.SKU = "B";
+            basketItem3.price = 60;
+            basket.BasketItemList.Add(basketItem3);
+
+            PromotionObj promotion = new PromotionObj();
+            List<PromotionType> PromotionList = new List<PromotionType>();
+            PromotionType promotionType = new PromotionType();
+            promotionType.operand = 20;
+            promotionType.operatorKey = new List<string>();
+            promotionType.operatorKey.Add("A");
+            promotionType.operatorType = "percentage";
+            promotionType.SKU_percentage = "A";
+            promotion.PromotionList.Add(promotionType);
+
+            List<SKU_Item> SKU_PriceList = new List<SKU_Item>();
+
+            SKU_Item SKU_Item1 = new SKU_Item();
+            SKU_Item1.SKU = "A";
+            SKU_Item1.price = 50;
+            SKU_PriceList.Add(SKU_Item1);
+
+            SKU_Item SKU_Item2 = new SKU_Item();
+            SKU_Item2.SKU = "B";
+            SKU_Item2.price = 60;
+            SKU_PriceList.Add(SKU_Item2);
+
+            SKU_Item SKU_Item3 = new SKU_Item();
+            SKU_Item3.SKU = "C";
+            SKU_Item3.price = 40;
+            SKU_PriceList.Add(SKU_Item3);
+
+            List<string> AppliedPromotions;
+            int total = promotion.ApplyPromotion(basket, promotion.PromotionList, SKU_PriceList, out AppliedPromotions);
+
+            Assert.AreEqual(120, total);
+            //Assert.ThrowsException<NullReferenceException>(() => promotion.ApplyPromotion(basket, promotion.PromotionList, SKU_PriceList, out AppliedPromotions));
+
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            Basket basket = new Basket();
+            BasketItem basketItem1 = new BasketItem();
+            basketItem1.SKU = "A";
+            basketItem1.price = 50;
+            basket.BasketItemList.Add(basketItem1);
+
+            BasketItem basketItem2 = new BasketItem();
+            basketItem2.SKU = "A";
+            basketItem2.price = 50;
+            basket.BasketItemList.Add(basketItem2);
+
+            BasketItem basketItem3 = new BasketItem();
+            basketItem3.SKU = "B";
+            basketItem3.price = 60;
+            basket.BasketItemList.Add(basketItem3);
+
+            PromotionObj promotion = new PromotionObj();
+            List<PromotionType> PromotionList = new List<PromotionType>();
+            PromotionType promotionType = new PromotionType();
+            promotionType.operand = 50;
+            promotionType.operatorKey = new List<string>();
+            promotionType.operatorKey.Add("A");
+            promotionType.operatorKey.Add("A");
+            promotionType.operatorKey.Add("B");
+            promotionType.operatorType = "static";
+            promotionType.SKU_percentage = "";
+            promotion.PromotionList.Add(promotionType);
+
+            List<SKU_Item> SKU_PriceList = new List<SKU_Item>();
+
+            SKU_Item SKU_Item1 = new SKU_Item();
+            SKU_Item1.SKU = "A";
+            SKU_Item1.price = 50;
+            SKU_PriceList.Add(SKU_Item1);
+
+            SKU_Item SKU_Item2 = new SKU_Item();
+            SKU_Item2.SKU = "B";
+            SKU_Item2.price = 60;
+            SKU_PriceList.Add(SKU_Item2);
+
+            SKU_Item SKU_Item3 = new SKU_Item();
+            SKU_Item3.SKU = "C";
+            SKU_Item3.price = 40;
+            SKU_PriceList.Add(SKU_Item3);
+
+            List<string> AppliedPromotions;
+            int total = promotion.ApplyPromotion(basket, promotion.PromotionList, SKU_PriceList, out AppliedPromotions);
+
+            Assert.AreEqual(50, total);
+            //Assert.ThrowsException<NullReferenceException>(() => promotion.ApplyPromotion(basket, promotion.PromotionList, SKU_PriceList, out AppliedPromotions));
+
+        }
     }
 }
